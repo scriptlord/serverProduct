90856799a25cacdd2ea0b5b5f885bce512bcdb44
Foluso Kayode <55621884+scriptlord@users.noreply.github.com>
2021-11-16 15:23:19 -0800

    delete me

--- passenger/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  roots: ['<rootDir>/workdir'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$'
-}
--- passenger/workdir/__test__/mock.ts
+++
@@ -1,4 +0,0 @@
-export const prefilled = Array.from({ length: 50 }).map((pass, ind) => ({
-    name: `${"passenger" + (ind + 1)}`,
-    location: expect.any(String),
-  }));
